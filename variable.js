a6d0a9a7eec1f8101c2844c820101d02dc4a4354
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-08 00:11:04 +0530

    started ts

---
+++ variable.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Thiru";
+console.log(greetings);
+// number integer or float its still a number 💪
+var userId = 334466.3;
+// boolean
+var isLoggedIn = false;
---
+++ variable.ts
@@ -0,0 +1,18 @@
+let greetings: string = "Hello Thiru";
+
+
+console.log(greetings);
+
+// number integer or float its still a number 💪
+
+let userId: number
+userId = 33445.5
+
+
+// boolean
+let isLoggedIn: boolean = false
+
+
+
+
+export {}
