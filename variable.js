b21ef7dce615ec7a270ddada0d971a75ca4bce92
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-08 00:12:03 +0530

    rm

--- variable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var greetings = "Hello Thiru";
-console.log(greetings);
-// number integer or float its still a number 💪
-var userId = 334466.3;
-// boolean
-var isLoggedIn = false;
